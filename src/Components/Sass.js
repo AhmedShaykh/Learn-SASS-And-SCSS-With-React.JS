1dbc8f22a05460ed2e9b3d837ac4e13c658af319
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-20 15:41:24 +0500

    It's Time To Push !

---
+++ src/Components/Sass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sass() {
+    return (
+        <div>
+            <h1 className='head'>
+                Learning SASS And SCSS With React.JS!
+            </h1>
+        </div>
+    )
+}
+
+export default Sass;
