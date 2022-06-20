fc4091aae600b577ec1fca10e5c5815c047d7170
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-20 17:36:13 +0500

    It's Time To Push !

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Sass from "./Components/Sass";
 import './Sass/App.css';
 
 function App() {
   return (
-    <Sass />
+    <div className='header'>
+      <h3 className='head'>
+        Learn SASS And SCSS In React.JS
+      </h3>
+      <p>Buy & Sell Bitcoin</p>
+    </div>
   );
 }
 
